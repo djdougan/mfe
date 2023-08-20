14a8af363ea80cafd7105ea213ba82c5bf1c7607
djdougan <djdougan@gmail.com>
2023-08-20 15:03:02 -0600

    added explamation marks

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './container/MarketingApp';
 
 export default () => {
   return <div>
-    <h1>Hi There!</h1>
+    <h1>Hi There!!!</h1>
     <hr />
     <MarketingApp /></div>;
 };
