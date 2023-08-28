207e500055a17f2c2a56f0d737e97343f045f41f
djdougan <djdougan@gmail.com>
2023-08-27 19:24:42 -0600

    Completed Auth

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,7 +4,7 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
   const history = defaultHistory || createMemoryHistory({
     initialEntries: [initialPath]
   });
@@ -13,12 +13,11 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
-
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
@@ -29,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
@@ -39,4 +38,3 @@ if (process.env.NODE_ENV === 'development') {
 // We are running through container
 // and we should export the mount function
 export { mount };
- 
